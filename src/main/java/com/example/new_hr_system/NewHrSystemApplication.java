4864be99831a8604d9b9bb9892b748bdda1a7d76
roger <roger@LAPTOP-QLMMS84C>
2022-11-30 17:35:07 +0800

    Second commit

---
+++ src/main/java/com/example/new_hr_system/NewHrSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_hr_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHrSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewHrSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_hr_system/NewHrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_hr_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewHrSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
