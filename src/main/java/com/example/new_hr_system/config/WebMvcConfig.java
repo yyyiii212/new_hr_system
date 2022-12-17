01f3c1763b3d8457217259e262d82161d5be3d83
user <user@MSI>
2022-12-17 18:05:09 +0800

    12/17 add config

---
+++ src/main/java/com/example/new_hr_system/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.new_hr_system.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer{
+	//用來連接前端與後端(暫存httpsession)所用方法
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+			.allowedOriginPatterns("*")
+			.allowCredentials(true)
+			.allowedMethods("POST")
+			.allowedHeaders("*");
+		}
+
+}
