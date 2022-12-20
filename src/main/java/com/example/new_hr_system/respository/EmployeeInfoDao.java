a16c0bdafd7df04b7ed9ccbca003a0cac5e8e425
roger <roger@LAPTOP-QLMMS84C>
2022-12-20 09:52:37 +0800

    12/20 09:53

--- src/main/java/com/example/new_hr_system/respository/EmployeeInfoDao.java
+++ src/main/java/com/example/new_hr_system/respository/EmployeeInfoDao.java
@@ -1,5 +1,7 @@
 package com.example.new_hr_system.respository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.new_hr_system.entity.EmployeeInfo;
 
 @Repository
 public interface EmployeeInfoDao extends JpaRepository<EmployeeInfo, String> {
-
+	
+	public List<EmployeeInfo> findAllById(String Id);
 }
